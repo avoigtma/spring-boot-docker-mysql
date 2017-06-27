c51955e221f56715f5641ac8d632e78dfcb560e7
Andreas Voigtmann <avoigtma@users.noreply.github.com>
2017-06-27 17:38:43 +0200

    Update database contents

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -5,4 +5,13 @@ CREATE TABLE person (
 	PRIMARY KEY (id)
 );
 
-insert into person (first_name, last_name) values ('Yuan', 'Ji');
+insert into person (first_name, last_name) values ('Donald', 'Duck');
+insert into person (first_name, last_name) values ('Donald', 'Trump');
+insert into person (first_name, last_name) values ('Thomas', 'Mann');
+insert into person (first_name, last_name) values ('Joe', 'Cocker');
+insert into person (first_name, last_name) values ('Wladimir', 'Putin');
+insert into person (first_name, last_name) values ('Angela', 'Merkel');
+insert into person (first_name, last_name) values ('Onkel', 'Dagobert');
+insert into person (first_name, last_name) values ('Mickey', 'Mouse');
+insert into person (first_name, last_name) values ('Gary', 'Moore');
+insert into person (first_name, last_name) values ('John', 'Doe');
